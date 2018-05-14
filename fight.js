ae89db2dfe3d7bf79a48311563fa82bcf7b3afda
serhii-horobets <serhii.horobets@gmail.com>
2018-05-14 22:15:04 +0300

    add fight

--- fight.js
+++ fight.js
@@ -13,8 +13,7 @@ class Fighter {
         if (this.health > 0) {
             console.log(`${this.name}'s health: ${this.health}`);
         } else {
-            this.health = 0;
-            console.log(`${this.name} is dead`);
+            console.log(`${this.name} is dead, health: ${this.health}`);
         }
     }
 
@@ -37,7 +36,56 @@ class ImprovedFighter extends Fighter {
 
 }
 
+let fight = (fighter, improvedFighter, ...point) => {
+    console.log('===Start the fight!!!===');
+    for (let i = 0; i < point.length; i++) {
+        //якщо бійці живі
+        if (improvedFighter.health > 0 && fighter.health > 0) {
+            console.log(`Results of the round #${i + 1}:`);
+            fighter.hit(improvedFighter, point[i]);
+            //якщо improvedFighter живий після отримання удару, то він б'є у відповідь
+            if (improvedFighter.health > 0) {
+                improvedFighter.hit(fighter, point[i]);
+            }
+            //якщо improvedFighter помер від удару fighter, виводимо fighter.health для завершеності "картини" (так як удару не буде і setDamage не буде викликатись)
+            else {
+                console.log(`${fighter.name}, health: ${fighter.health}`);
+            }
+        }
+        //якщо хтось помер 
+        if (improvedFighter.health <= 0 || fighter.health <= 0) {
+            let winner = fighter.health <= 0 ? improvedFighter.name : fighter.name
+            console.log(`The battle is over, ${winner} won!`);
+            return;
+        }
+    }
+    console.log('The battle is over. No deaths');
+}
 
+//Функція для рандомного (50/50) вибору того, кто перший починає бій. Будемо використовувати її у сортуванні масиву бійців arr.sort
+function compareRandom() {
+   /** 
+    * У змінну rand генеруєм true/false
+    * 0.495 тому що Math.random генерує з діапазону [0,1) , а треба щоб true/false генерувались як можна ближче до 50/50 відсотків
+    * via https://books.google.com.ua/books?id=G14FCgAAQBAJ&lpg=PA91&hl=uk&pg=PA91#v=onepage&q&f=false
+    */
+    let rand = Math.random() > 0.495;
+    if (rand) {
+        return 1;
+    } else {
+        return -1;
+    }
+}
+
+//Встановіть name, power>0, health>0 у змінних бійців
+let Kombo = new Fighter('Kombat', 4, 15);
+let Rambo = new ImprovedFighter('Rambo', 2, 20);
+
+let arr = [Kombo, Rambo];
+arr.sort(compareRandom); //рандомно розміщюємо бійців
+
+//Вкажіть розмір point у раундах
+fight(...arr, 2, 4, 5);
 
 
 
